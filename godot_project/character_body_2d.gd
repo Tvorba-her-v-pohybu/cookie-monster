33a0c0f220f668432656ee1d279366017ff56c61
Jakub Doucek <jakubdoucek@gmail.com>
2025-01-20 16:26:26 +0100

    Cookie attack

--- godot_project/character_body_2d.gd
+++ godot_project/character_body_2d.gd
@@ -6,6 +6,16 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack"):
+		start_attack()
+		
+func start_attack():
+	if %AttackDelayTimer.is_stopped():
+		enemies_killed = 0
+		%AttackAnimationPlayer.play("attack")
+		%AttackDelayTimer.start()
+
 func process_damage(value: int):
 	$AnimationPlayer.play("dmg")
 	health -= value
@@ -17,8 +27,16 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	var direction_up := Input.get_axis("ui_up", "ui_down")
+	
+	look_at(global_position + Vector2(direction, direction_up))
 
 	velocity.x = direction * SPEED
 	velocity.y = direction_up * SPEED
 
 	move_and_slide()
+
+var enemies_killed := 0
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy") and not %AttackDelayTimer.is_stopped() and enemies_killed < 1:
+		enemies_killed += 1
+		body.queue_free()
