7fe5d2e96e8688b6d3721eb80b823371b8cc16fd
Jakub Doucek <jakubdoucek@gmail.com>
2025-02-09 20:36:21 +0100

    BOSS

--- godot_project/boss/boss.gd
+++ godot_project/boss/boss.gd
@@ -14,3 +14,7 @@ func _on_shoot_timer_timeout() -> void:
 	var dudl = load("res://boss/dudl.tscn").instantiate()
 	add_child(dudl)
 	dudl.position = Vector2(-116, 137)
+
+
+func process_damage(value: int):
+	queue_free()
