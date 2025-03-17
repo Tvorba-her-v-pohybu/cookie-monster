ba0b994f72d8abb0ee527479c84e635392a4d835
Bor3c <borek.Holenda@gmail.com>
2025-03-17 16:29:24 +0100

    smrt

--- godot_project/canvas_layer.gd
+++ godot_project/canvas_layer.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-
+var smrt := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	%Healthbar.value = %CharacterBody2D.health
+	
+	if %CharacterBody2D.health <= 0 and not smrt:
+		%smrtLbl.visible = true
+		%KoupilJsemTo.play()
+		smrt = true
+	
