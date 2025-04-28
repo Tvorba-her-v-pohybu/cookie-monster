aed7c3d8df9ee5d71d7afdc3c730a366b74a038a
MIKYYYYYYYYYYYYYYYYYYYYY <mikulas.doucek@gmail.com>
2025-04-28 17:03:49 +0200

    backroom

--- godot_project/backroom.gd
+++ godot_project/backroom.gd
@@ -3,15 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	%CharacterBody2D.top_down = true
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_propast_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		$propast/Dira.visible = true
-		$propast/AnimationPlayer.play("propast")
