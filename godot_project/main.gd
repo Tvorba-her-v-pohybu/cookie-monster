6dac15a39b5b8c768d5eb1c3dda822714970d99d
Jakub Doucek <jakubdoucek@gmail.com>
2025-05-18 16:52:01 +0200

    Miky upravy

--- godot_project/main.gd
+++ godot_project/main.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@export var propast_points : Array[Node2D] = [] 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var index = randi_range(0, propast_points.size()-1)
+	%propast.global_position = propast_points[index].global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
